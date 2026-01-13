1b9268008d6e4975f007a863f53ac104c13ef0b7
Bakoz <bakozderabi@gmail.com>
2026-01-13 13:53:07 +0100

    added more infos about the script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'void_gr20'
+description 'Dies ist ein anpassbares HUD für FiveM/ESX mit vielen Features wie Speedometer, Statusanzeigen, Postleitzahlen, Benachrichtigungen, Fortschrittsbalken, Radio-Liste und mehr. - Only for esx asap for qbox and qbcore soon'
+
 client_script 'client/client.lua'
 server_script 'server/server.lua'
 
